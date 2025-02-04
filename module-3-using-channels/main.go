3561fdfd59d3fe079eae185b3a259af28a761341
Subodh Khanal <skhanal5@terpmail.umd.edu>
2025-02-03 19:37:44 -0500

    Add notes to module 3. Add module 4 dir

--- module-3-using-channels/main.go
+++ module-3-using-channels/main.go
@@ -13,9 +13,16 @@ func bar(ch chan bool) {
 	fmt.Println("In function: bar()")
 	ch <- true
 }
-
+/*
+	Same as module 2. Why?
+*/
 func main() {
-	ch := make(chan bool)
+	// Using unbuffered channel.
+	// For each write to this channel, each writing
+	// goroutine pauses until another goroutine reads 
+	// Same behavior for writes.
+	// Thus, for a read/write to happen -> we need two concurrently running goroutines
+	ch := make(chan bool) 
 	go foo(ch)
 	<- ch
 	go bar(ch)
